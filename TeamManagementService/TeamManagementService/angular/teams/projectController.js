af1e50435609c2102b5569c5e7fd6e8d888e46b0
Dawid Swietoslawski <dswietoslawski@gmail.com>
2016-08-20 18:05:10 +0200

    team to project refactor angular side

---
+++ TeamManagementService/TeamManagementService/angular/teams/projectController.js
@@ -0,0 +1,30 @@
+﻿var app = angular.module('tmsApp');
+
+app.controller('projectController', ['$scope', '$rootScope', 'projectService', 'appService', function ($scope, $rootScope, projectService, appService) {
+    $scope.newProject = {
+        name: "",
+        adminId: 0
+    }
+
+    $scope.selectedProject = "select a project";
+
+    $scope.projects = [];
+
+    var init = function () {
+        projectService.get().then(function (response) {
+            $scope.projects = response;
+        });
+    }
+
+    init();
+
+    $scope.add = function (project) {
+        project.adminId = appService.getLoginInfo().id;
+        projectService.add(project);
+    };
+
+    $scope.selectProject = function (project) {
+        $scope.selectedProject = project.name;
+        $scope.$broadcast('selected-project-changed', project);
+    }
+}]);
---
+++ TeamManagementService/TeamManagementService/angular/teams/projectService.js
@@ -0,0 +1,33 @@
+﻿var app = angular.module('tmsApp');
+
+app.service('projectService', ['$http', '$q', function ($http, $q) {
+
+    var config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    this.add = function (team) {
+        var response = $http.post('http://localhost:63601/api/teams', team, config)
+        return response;
+    }
+
+    this.get = function () {
+        var def = $q.defer();
+
+        var response = $http.get('http://localhost:63601/api/teams', config)
+        .success(function (response) {
+            def.resolve(response);
+        }).error(function (error) {
+            def.reject("Error");
+        });
+
+        return def.promise;
+    }
+
+    this.getById = function (id) {
+        var response = $http.get('http://localhost:63601/api/teams' + id, user, config)
+        return response;
+    }
+}]);
