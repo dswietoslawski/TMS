23f7a97d8a6b854da2fd83153276960225b684b6
Dawid Swietoslawski <dswietoslawski@gmail.com>
2016-09-11 18:25:26 +0200

    Register modal added

---
+++ TeamManagementService/TeamManagementService/angular/users/register/registerController.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module('tmsApp');
+
+app.controller('registerController', ['$scope', '$window', 'userService', 'appService', '$uibModal', function ($scope, $window, userService, appService, $uibModal) {
+    $scope.open = function (size) {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: '../users/register/registerModal.html',
+            controller: 'registerModalController',
+            controllerAs:'registerModalCtrl',
+            size: size,
+            resolve: {
+                items: function () {
+
+                }
+            }
+        });
+
+    };
+}]);
--- TeamManagementService/TeamManagementService/angular/users/userController.js
+++ TeamManagementService/TeamManagementService/angular/users/userController.js
@@ -1,23 +1,5 @@
 ﻿var app = angular.module('tmsApp');
 
 app.controller('userController', ['$scope', 'userService', 'appService', function ($scope, userService, appService) {
-    $scope.registerUser = {
-        userName: "",
-        password: "",
-        confirmPassword: "",
-        email: "",
-        firstName: "",
-        lastName: ""
-    };
-    $scope.userMsg = "";
 
-    $scope.register = function (user) {
-        userService.register(user)
-            .then(function (response) {
-                $scope.userMsg = response;
-            },
-            function (error) {
-                $scope.userMsg = error;
-            });// get a promise use login function if SUCCESS
-    };
 }]);
