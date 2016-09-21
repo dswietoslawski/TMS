53dd704979e77bec15bef6e4cba6dd469b123b1f
Dawid Swietoslawski <dswietoslawski@gmail.com>
2016-09-21 21:01:01 +0200

    Commit off canvas

--- TeamManagementService/TeamManagementService/angular/app.js
+++ TeamManagementService/TeamManagementService/angular/app.js
@@ -1,5 +1,14 @@
-﻿var app = angular.module('tmsApp', ['LocalStorageModule', 'ngAnimate', 'ngSanitize','ngAria', 'ngMaterial', 'ngMessages', 'ui.bootstrap', 'ui.sortable']);
+﻿var app = angular.module('tmsApp', ['LocalStorageModule', 'ngAnimate', 'ngSanitize','ngAria', 'ngMaterial', 'ngMessages', 'ui.bootstrap', 'ui.sortable', 'ngTouch', 'cn.offCanvas']);
 
 app.config(function ($animateProvider) {
     $animateProvider.classNameFilter(/^(?:(?!ng-animate-disabled).)*$/);
 })
+
+
+app.factory('offCanvas', function (cnOffCanvas) {
+    return cnOffCanvas({
+        controller: 'sidebarController',
+        controllerAs: 'sidebar',
+        templateUrl: '../sidebarMenu/sidebarMenu.html'
+    })
+});
